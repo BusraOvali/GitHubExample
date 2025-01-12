1849b5ad19580ae4cebfde9d87b6d67fccc08504
busraovali <busraovali89@gmail.com>
2025-01-12 15:05:13 -0600

    s added

--- src/test/java/com/cydeo/burakExample/Burak.java
+++ src/test/java/com/cydeo/burakExample/Burak.java
@@ -3,8 +3,10 @@ package com.cydeo.burakExample;
 public class Burak {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int s=0;
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            System.out.println(s++);
         }
     }
 
