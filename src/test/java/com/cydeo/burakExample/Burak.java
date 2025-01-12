b47b20ce85b935e9da4a1eb6f7c0c16c5d260b4c
busraovali <busraovali89@gmail.com>
2025-01-12 15:03:02 -0600

    for loop added

--- src/test/java/com/cydeo/burakExample/Burak.java
+++ src/test/java/com/cydeo/burakExample/Burak.java
@@ -3,5 +3,9 @@ package com.cydeo.burakExample;
 public class Burak {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
+
 }
